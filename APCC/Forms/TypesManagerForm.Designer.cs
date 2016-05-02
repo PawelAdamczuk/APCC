f05f8c34ff6d1db4eccd0cf811002adcf5c110e4
Foka <rakfrag@gmail.com>
2016-05-02 23:38:56 +0200

    Added TypesManagerForm.

---
+++ APCC/Forms/TypesManagerForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace APCC.Forms
+{
+    partial class TypesManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvTypes = new System.Windows.Forms.DataGridView();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTypes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTypes
+            // 
+            this.dgvTypes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTypes.Location = new System.Drawing.Point(12, 12);
+            this.dgvTypes.Name = "dgvTypes";
+            this.dgvTypes.RowTemplate.Height = 24;
+            this.dgvTypes.Size = new System.Drawing.Size(240, 415);
+            this.dgvTypes.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(258, 379);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(174, 48);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(258, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(174, 48);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(258, 66);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(174, 48);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(258, 120);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(174, 48);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // TypesManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 439);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.dgvTypes);
+            this.Name = "TypesManagerForm";
+            this.Text = "Components types manager";
+            this.Load += new System.EventHandler(this.TypesManagerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTypes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvTypes;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ APCC/Forms/TypesManagerForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APCC.Forms
+{
+    public partial class TypesManagerForm : Form
+    {
+        public TypesManagerForm()
+        {
+            InitializeComponent();
+        }
+
+        // Load dataGridView for ComponentTypes
+        private void loadDataGrid()
+        {
+            string lStmt;
+
+            try
+            {
+                lStmt = @"
+                            select
+                                typID,
+                                typName
+                            from
+                                TYPES
+                         ";
+
+                SqlCommand lCommand = new SqlCommand(lStmt, SqlConn.Connection);
+                SqlDataReader lDataReader = lCommand.ExecuteReader();
+
+                DataTable lTable = new DataTable();
+                lTable.Load(lDataReader);
+
+                dgvTypes.DataSource = lTable;
+
+                dgvTypes.Columns[0].HeaderText = "ID";
+                dgvTypes.Columns[1].HeaderText = "Name";
+
+                dgvTypes.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                dgvTypes.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        // Refresh dataGrid
+        public void refreshGrid()
+        {
+            this.loadDataGrid();
+            dgvTypes.AutoResizeColumns();
+        }
+
+        // On Load
+        private void TypesManagerForm_Load(object sender, EventArgs e)
+        {
+            this.loadDataGrid();
+            dgvTypes.AutoResizeColumns();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
