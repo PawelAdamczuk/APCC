7df9d8d7a53fa7a8f08d5d11ca07b2dbc89b4a05
Tomasz Adam Skrzypczak <tomasz.adam.skrzypczak@gmail.com>
2016-05-06 00:35:14 +0200

    UserManager up #2

--- APCC/Forms/UsersManageForm.cs
+++ APCC/Forms/UsersManageForm.cs
@@ -13,6 +13,9 @@ namespace APCC.Forms
 {
     public partial class UsersManageForm : Form
     {
+
+        private int lastId;
+
         public UsersManageForm()
         {
             InitializeComponent();
@@ -24,27 +27,65 @@ namespace APCC.Forms
             {
                 SqlCommand giveMeUsers = new SqlCommand("SELECT * FROM [dbo].[UsersManage]", SqlConn.Connection);
 
-                SqlDataReader reader = giveMeUsers.ExecuteReader();
-
                 int id;
                 string fName, sName, rName;
-
-                while (reader.Read())
+                using (SqlDataReader reader = giveMeUsers.ExecuteReader())
                 {
-                    id = (int)reader["usrID"];
-                    fName = (string)reader["usrFName"];
-                    sName = (string)reader["usrSName"];
-                    rName = (string)reader["rlsName"];
+                    while (reader.Read())
+                    {
+                        id = (int)reader["usrID"];
+                        fName = (string)reader["usrFName"];
+                        sName = (string)reader["usrSName"];
+                        rName = (string)reader["rlsName"];
 
-                    listBox1.Items.Add(id + " " + fName + " " + sName + " " + rName + "\n");
+                        listBox1.Items.Add(id + " " + fName + " " + sName + " " + rName + "\n");
+                    }
+
                 }
-
-                reader.Close();
 
             }catch(Exception ex)
             {
                 listBox1.Items.Add(ex.Message.ToString());
             }
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selectedUser = (sender as ListBox).SelectedItem;
+
+            if(selectedUser != null)
+            {
+                int idUsr = Int32.Parse(selectedUser.ToString().Split(' ')[0]);
+
+                if(idUsr != lastId)
+                {
+                    listBox2.Items.Clear();
+                    using (SqlCommand giveMeBuilds = new SqlCommand("getUsrBuilds", SqlConn.Connection) { CommandType = CommandType.StoredProcedure })
+                    {
+                        giveMeBuilds.Parameters.Add("@uID", SqlDbType.Int);
+                        giveMeBuilds.Parameters["@uID"].Value = idUsr;
+                        using(SqlDataReader buildReader = giveMeBuilds.ExecuteReader())
+                        {
+                            while (buildReader.Read())
+                            {
+                                listBox2.Items.Add((int)buildReader["bldID"]);
+                            }
+                        }
+                    }
+                      
+                }
+            }
+       }
+
     }
 }
