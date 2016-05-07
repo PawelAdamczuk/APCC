8657a870ed1beebaf6b46cb78c5f6fc869a485a4
PawelAdamczuk <p.m.adamczuk@gmail.com>
2016-05-07 21:11:01 +0200

    Added the AddComponentForm form

---
+++ APCC/Forms/AddComponentForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APCC.Forms
+{
+    public partial class AddComponentForm : Form
+    {
+        ArrayList types;
+
+
+        public AddComponentForm()
+        {
+            InitializeComponent();
+
+            types = new ArrayList();
+
+            string connString = String.Format("SELECT * FROM TYPES");
+            SqlCommand cmd = new SqlCommand(connString, SqlConn.Connection);
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    
+                }
+            }
+
+
+
+
+
+
+        }
+    }
+}
