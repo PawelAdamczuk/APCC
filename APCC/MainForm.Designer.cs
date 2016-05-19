3ce245e1a31e6063773f49f3d6341f0ec42eec79
Tomasz Adam Skrzypczak <tomasz.adam.skrzypczak@gmail.com>
2016-05-19 19:55:43 +0200

    deleting build2

--- APCC/MainForm.Designer.cs
+++ APCC/MainForm.Designer.cs
@@ -32,7 +32,6 @@
             this.menuStrip = new System.Windows.Forms.MenuStrip();
             this.menuShow = new System.Windows.Forms.ToolStripMenuItem();
             this.buildsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.builds2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.componentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuAdmin = new System.Windows.Forms.ToolStripMenuItem();
             this.usersToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
@@ -59,8 +58,8 @@
             this.toolStripMenuItem_LogOut});
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
-            this.menuStrip.Padding = new System.Windows.Forms.Padding(11, 2, 0, 2);
-            this.menuStrip.Size = new System.Drawing.Size(1236, 28);
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip.Size = new System.Drawing.Size(927, 24);
             this.menuStrip.TabIndex = 0;
             this.menuStrip.Text = "MenuStrip";
             // 
@@ -68,32 +67,24 @@
             // 
             this.menuShow.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.buildsToolStripMenuItem,
-            this.builds2ToolStripMenuItem,
             this.componentsToolStripMenuItem});
             this.menuShow.ImageTransparentColor = System.Drawing.SystemColors.ActiveBorder;
             this.menuShow.Name = "menuShow";
-            this.menuShow.Size = new System.Drawing.Size(57, 24);
+            this.menuShow.Size = new System.Drawing.Size(48, 20);
             this.menuShow.Text = "&Show";
             this.menuShow.Click += new System.EventHandler(this.fileMenu_Click);
             // 
             // buildsToolStripMenuItem
             // 
             this.buildsToolStripMenuItem.Name = "buildsToolStripMenuItem";
-            this.buildsToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.buildsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.buildsToolStripMenuItem.Text = "Builds";
             this.buildsToolStripMenuItem.Click += new System.EventHandler(this.buildsToolStripMenuItem_Click);
-            // 
-            // builds2ToolStripMenuItem
-            // 
-            this.builds2ToolStripMenuItem.Name = "builds2ToolStripMenuItem";
-            this.builds2ToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
-            this.builds2ToolStripMenuItem.Text = "Builds2";
-            this.builds2ToolStripMenuItem.Click += new System.EventHandler(this.buildToolStripMenuItem_Click);
             // 
             // componentsToolStripMenuItem
             // 
             this.componentsToolStripMenuItem.Name = "componentsToolStripMenuItem";
-            this.componentsToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.componentsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.componentsToolStripMenuItem.Text = "Components";
             this.componentsToolStripMenuItem.Click += new System.EventHandler(this.componentsToolStripMenuItem_Click);
             // 
@@ -103,20 +94,20 @@
             this.usersToolStripMenuItem1,
             this.componentTypesToolStripMenuItem});
             this.menuAdmin.Name = "menuAdmin";
-            this.menuAdmin.Size = new System.Drawing.Size(119, 24);
+            this.menuAdmin.Size = new System.Drawing.Size(98, 20);
             this.menuAdmin.Text = "Administration";
             // 
             // usersToolStripMenuItem1
             // 
             this.usersToolStripMenuItem1.Name = "usersToolStripMenuItem1";
-            this.usersToolStripMenuItem1.Size = new System.Drawing.Size(201, 26);
+            this.usersToolStripMenuItem1.Size = new System.Drawing.Size(169, 22);
             this.usersToolStripMenuItem1.Text = "Users";
             this.usersToolStripMenuItem1.Click += new System.EventHandler(this.usersToolStripMenuItem_Click);
             // 
             // componentTypesToolStripMenuItem
             // 
             this.componentTypesToolStripMenuItem.Name = "componentTypesToolStripMenuItem";
-            this.componentTypesToolStripMenuItem.Size = new System.Drawing.Size(201, 26);
+            this.componentTypesToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
             this.componentTypesToolStripMenuItem.Text = "Component types";
             this.componentTypesToolStripMenuItem.Click += new System.EventHandler(this.componentTypeToolStripMenuItem_Click);
             // 
@@ -125,13 +116,13 @@
             this.manuTools.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.optionsToolStripMenuItem});
             this.manuTools.Name = "manuTools";
-            this.manuTools.Size = new System.Drawing.Size(57, 24);
+            this.manuTools.Size = new System.Drawing.Size(47, 20);
             this.manuTools.Text = "&Tools";
             // 
             // optionsToolStripMenuItem
             // 
             this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
-            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
             this.optionsToolStripMenuItem.Text = "&Options";
             // 
             // toolStripMenuItem_login
@@ -139,7 +130,7 @@
             this.toolStripMenuItem_login.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.toolStripMenuItem_login.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.toolStripMenuItem_login.Name = "toolStripMenuItem_login";
-            this.toolStripMenuItem_login.Size = new System.Drawing.Size(62, 24);
+            this.toolStripMenuItem_login.Size = new System.Drawing.Size(52, 20);
             this.toolStripMenuItem_login.Text = "Log in";
             this.toolStripMenuItem_login.Click += new System.EventHandler(this.toolStripMenuItem_login_Click);
             // 
@@ -148,7 +139,7 @@
             this.toolStripMenuItem_LogOut.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.toolStripMenuItem_LogOut.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.toolStripMenuItem_LogOut.Name = "toolStripMenuItem_LogOut";
-            this.toolStripMenuItem_LogOut.Size = new System.Drawing.Size(72, 24);
+            this.toolStripMenuItem_LogOut.Size = new System.Drawing.Size(60, 20);
             this.toolStripMenuItem_LogOut.Text = "Log out";
             this.toolStripMenuItem_LogOut.Click += new System.EventHandler(this.toolStripMenuItem_LogOut_Click);
             // 
@@ -157,29 +148,29 @@
             this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel});
-            this.statusStrip.Location = new System.Drawing.Point(0, 683);
+            this.statusStrip.Location = new System.Drawing.Point(0, 553);
             this.statusStrip.Name = "statusStrip";
-            this.statusStrip.Padding = new System.Windows.Forms.Padding(1, 0, 25, 0);
-            this.statusStrip.Size = new System.Drawing.Size(1236, 25);
+            this.statusStrip.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip.Size = new System.Drawing.Size(927, 22);
             this.statusStrip.TabIndex = 2;
             this.statusStrip.Text = "StatusStrip";
             // 
             // toolStripStatusLabel
             // 
             this.toolStripStatusLabel.Name = "toolStripStatusLabel";
-            this.toolStripStatusLabel.Size = new System.Drawing.Size(49, 20);
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(39, 17);
             this.toolStripStatusLabel.Text = "Status";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1236, 708);
+            this.ClientSize = new System.Drawing.Size(927, 575);
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.menuStrip);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip;
-            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "APCC";
@@ -206,7 +197,6 @@
         public System.Windows.Forms.StatusStrip statusStrip;
         public System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem_LogOut;
-        private System.Windows.Forms.ToolStripMenuItem builds2ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem menuAdmin;
         private System.Windows.Forms.ToolStripMenuItem usersToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem componentTypesToolStripMenuItem;
